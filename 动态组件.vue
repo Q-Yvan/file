8daee222615f6344685ff121368bee49ef1eb7c6
Q-Yvan <54618855+Q-Yvan@users.noreply.github.com>
2021-11-17 19:17:55 +0800

    Add files via upload

---
+++ 动态组件.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-for="val in componentsData" :key="val.id">
+    <component :is="val.type" />
+  </div>
+</template>
+<script>
+import CustomTitle from './CustomTitle'
+import CustomText from './CustomText'
+import CustomImage from './CustomImage'
+
+export default {
+  data() {
+    return {
+      componentsData: [
+        {
+          id: 1,
+          type: 'CustomTitle'
+        },
+        {
+          id: 2,
+          type: 'CustomText'
+        },
+        {
+          id: 3
+          type: 'CustomImage'
+        }
+      ]
+    }
+  }
+}
+</script>
